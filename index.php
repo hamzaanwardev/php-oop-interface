5f045f555dfb61f82fd378db5fabdbe94c5f4591
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-04-02 23:49:18 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     
+  
+<h1>PHP OOP Interface</h1>
+<p>Interface are used to inherite more than one classes (properties and methods) in a class. </p>
+<p>Interface exactly works like Abstract Class. So we can not make its own Object.</p>
+<p>we can not either make properties or variables in interface and can not assign them value.</p>
+<p>In interface we can declare methods but we can not implement them. we use them for drived class.</p>
+
+
 </body>
 </html>
